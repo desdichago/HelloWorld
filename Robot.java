664ff49ff49b0ea5a300733dc3f79a4501bbda6f
Alex F <desdichago@users.noreply.github.com>
2020-06-24 15:11:43 +0300

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,33 @@
+package lulz;
+
+public class Robot {
+    static String weigth = "A lot";
+    /* default */ double ageMonth = 5, ageDays = 2;
+    private static boolean success = true;
+
+    public void main(String[] args) {
+        final String retries = "1";
+        // P1
+    }
+}
+
+
+/**
+ * Given the following application, fill in the missing values in the table starting from the top and going downward.
+ * Number of Variables Accessible at P1
+ * Class:    ___
+ * Instance: ___
+ * Local:    ___
+ *
+ * A: 2,0,1
+ * B: 2,2,1
+ * C: 1,0,1
+ * D: 0,2,1
+ */
+ 
+ /*
+ * Notice in this question that main() is not a static method,
+ * therefore it can access both class and instance variables.
+ * Since there are two class variables and two instance variables defined,
+ * Option B is the correct answer.
+ */
